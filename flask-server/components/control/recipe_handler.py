b9e2f047a4871bf6428b86fca5ccf8fc6f3e913a
daniafernandez <daniafernandez122@gmail.com>
2025-02-23 12:22:12 -0500

    implemented insert_recipe method

--- flask-server/components/control/recipe_handler.py
+++ flask-server/components/control/recipe_handler.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
 
 recipe_handler = Blueprint('recipe_handler', __name__)
@@ -35,3 +35,52 @@ def recipes():
             recipe_book.append({"name": name, "serves": serves, "ingredients": ingredients})
 
     return recipe_book
+
+@recipe_handler.route("/insertRecipe", methods=["POST"])
+@cross_origin()
+def insert_recipe():
+    data = request.get_json()
+
+    # Ensure required fields are present
+    if not data or "name" not in data or "serves" not in data or "ingredients" not in data:
+        return jsonify({"error": "Missing required fields"}), 400
+
+    name = data["name"]
+    serves = data["serves"]
+    ingredient_items = data["ingredients"]
+
+    existing_recipe = db.recipe_book.find_one({"name": name})
+
+    if existing_recipe:
+        return jsonify({"message": "A recipe with this name already exists."}), 409
+
+    ingredient_ids = []
+
+    for ingredient_item in ingredient_items:
+        ingredient_name = ingredient_item["name"]
+        ingredient_units = ingredient_item["units"]
+        ingredient_quantity = ingredient_item["quantity"]
+
+        ingredient = db.ingredient_store.find_one({"name": ingredient_name})
+        if not ingredient:
+            new_ingredient = {
+                "name": ingredient_name,
+                "units": ingredient_units
+            }
+            ingredient_inserted = db.ingredient_store.insert_one(new_ingredient)
+            ingredient_id = ingredient_inserted.inserted_id
+        else:
+            ingredient_id = ingredient["_id"]
+
+            # Append ingredient reference for the recipe
+        ingredient_ids.append({"ingredient_id": ingredient_id, "quantity": ingredient_quantity})
+
+    db.recipe_book.insert_one({
+        "name": name,
+        "serves": serves,
+        "ingredients": ingredient_ids
+    })
+
+    return jsonify({"message": "Recipe added successfully!"}), 201
+
+db = client.meal_planner
